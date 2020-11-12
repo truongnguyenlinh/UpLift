cba217df295e27552ebe65824fdff242efec96dc
krystal2694 <pwong2632my.bcit.ca>
2020-11-12 15:58:28 -0800

    make opening screen stay for 2 seconds

--- app/src/main/java/com/example/uplift/SplashActivity.java
+++ app/src/main/java/com/example/uplift/SplashActivity.java
@@ -5,11 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.concurrent.TimeUnit;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // make opening screen stay for 2 seconds
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
