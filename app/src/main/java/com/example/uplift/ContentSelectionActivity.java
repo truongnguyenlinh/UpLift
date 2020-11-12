5cf431d2c5cbcb28fce7c5a20a8117b26a88b127
Theoren Leveille <leveilletheoren@gmail.com>
2020-11-12 12:05:04 -0800

    Splash screen

---
+++ app/src/main/java/com/example/uplift/ContentSelectionActivity.java
@@ -0,0 +1,14 @@
+package com.example.uplift;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class ContentSelectionActivity extends ListActivity {
+
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+
+//        ArrayAdapter<Category>
+    }
+}
