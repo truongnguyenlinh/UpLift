42960bea2e51b46bd1d4483cc4b31a5992b819ec
krystal2694 <pwong2632my.bcit.ca>
2020-11-13 19:34:13 -0800

    added TestView for greeting

--- app/src/main/java/com/example/uplift/GetFrequencyActivity.java
+++ app/src/main/java/com/example/uplift/GetFrequencyActivity.java
@@ -4,13 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
