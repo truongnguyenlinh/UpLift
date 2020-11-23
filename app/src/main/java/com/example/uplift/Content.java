1ddab3c1794280d2cac995f74c56f8660544d536
krystal2694 <pwong2632my.bcit.ca>
2020-11-22 20:57:03 -0800

    Content class completed

--- app/src/main/java/com/example/uplift/Content.java
+++ app/src/main/java/com/example/uplift/Content.java
@@ -15,10 +15,50 @@ public static final ArrayList<String> wellness = new ArrayList<>(
 
 public static final ArrayList<String> goodNews = new ArrayList<>(
         Arrays.asList(
-                "Have you gone outside today? Don't forget to go get some fresh air and your daily dose of Vitamin D!",
-                "Now is not the time to be hard on yourself. You are more than capable, even if you can't always see it.",
-                "Don't forget to check in with your friends and family - it's important to stick together while we have to be apart!"
+                "Zoom is lifting its 40-minute time limit for Thanksgiving Day so families can hang out together.",
+                "After 45 years, the Gray Wolf has successfully been lifted off the US endangered species list.",
+                "Giuseppe Paternò has become Italy’s oldest graduate at the age of 96 after being awarded first-class honours in philosophy from the University of Palermo in Sicily."
         )
 );
 
+
+public static final ArrayList<String> exercise = new ArrayList<>(
+        Arrays.asList(
+                "Pace yourself. Have at least one recovery day each week to rest. If you are experiencing pain, rest until the pain has gone.",
+                "Stay hydrated. You can lose around one and a half litres of fluid for every hour of exercise; so drink water before, during and after a session.",
+                "Warm up and cool down. Try slow stretches and go through the motions of your sport or activity before starting. Cool down with slow stretching."
+        )
+);
+
+public static final ArrayList<String> animals = new ArrayList<>(
+        Arrays.asList(
+                "lamb.jpg",
+                "penguin.jpg",
+                "baby_seal.jpg",
+                "bunny.jpg",
+                "cat.jpg"
+        )
+);
+
+
+public static final ArrayList<String> travel = new ArrayList<>(
+        Arrays.asList(
+                "greece.jpg",
+                "iceland.jpg",
+                "maldives.jpg",
+                "whitsundays.jpg",
+                "travel1.jpg",
+                "travel2.jpg"
+        )
+);
+
+public static final ArrayList<String> nature = new ArrayList<>(
+        Arrays.asList(
+                "waterfall.jpg",
+                "joffre_lakes.jpg",
+                "northern_lights.jpg",
+                "chile.jpg",
+                "winter.jpg"
+        )
+);
 }
