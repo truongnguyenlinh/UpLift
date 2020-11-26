1e36b3fe4d1408ca3745c148db5f9523ac85c489
krystal2694 <pwong2632my.bcit.ca>
2020-11-25 17:20:22 -0800

    cleaned up Broadcast Receiver, changed notification image fit

--- app/src/main/java/com/example/uplift/MyBroadcastReceiver.java
+++ app/src/main/java/com/example/uplift/MyBroadcastReceiver.java
@@ -9,13 +9,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Bundle;
-import android.os.Vibrator;
 import android.util.Log;
 import android.widget.RemoteViews;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -28,7 +25,6 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.concurrent.Semaphore;
 
 public class MyBroadcastReceiver extends BroadcastReceiver {
 
@@ -39,29 +35,22 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     Context mContext;
     PendingIntent actionPendingIntent;
 
-    private FirebaseAuth firebaseAuth;
-    private DatabaseReference databaseReference;
-    private FirebaseUser user;
     ArrayList<String> categories;
-
-    private String name;
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        Log.e("ERROR", "In onReceive");
         categories = new ArrayList<>();
 
-        firebaseAuth=FirebaseAuth.getInstance();
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
         if (firebaseAuth.getCurrentUser() == null){
             mContext.startActivity(new Intent(mContext,SignInActivity.class));
         }
-        user = firebaseAuth.getCurrentUser();
-        databaseReference = FirebaseDatabase.getInstance().getReference().child(user.getUid()).child("categories/");
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child(user.getUid()).child("categories/");
 
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Log.e("ERROR", "In onDataChange");
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 try {
                     for (DataSnapshot postSnapShot: dataSnapshot.getChildren()) {
                         String category = postSnapShot.getValue(String.class);
@@ -75,7 +64,7 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
+            public void onCancelled(@NonNull DatabaseError databaseError) {
                 Log.d("ERROR", "The read failed: " + databaseError.getCode());
             }
         });
@@ -87,9 +76,7 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
         Intent actionIntent = new Intent(context, MainActivity.class);
         mContext = context;
 
-        actionPendingIntent = PendingIntent.getActivity(
-                context,
-                0,
+        actionPendingIntent = PendingIntent.getActivity(context, 0,
                 actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         notificationLayout = new RemoteViews("com.example.uplift",
@@ -106,23 +93,10 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
         // determine category
         String randomCategory = getRandomCategory();
 
+        // create notification
         Notification notification = isImageCategory(randomCategory) ? getImageNotification(randomCategory) : getTextNotification(randomCategory);
+        notifManager.createNotificationChannel(channel);
         notifManager.notify(NOTIFICATION_ID, notification);
-        Log.e("ERROR", "Notification sent");
-    }
-
-    public Notification getNotification() {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(mContext, NOTIFICATION_CHANNEL_ID)
-        .setSmallIcon(R.mipmap.ic_launcher)
-        .setContentTitle("UpLift")
-        .setContentText("Have you gone outside today? Fresh air boosts your serotonin for a better mood!")
-        .setPriority(NotificationCompat.PRIORITY_HIGH)
-        .setVibrate(new long[] {0, 1000})
-        .setContentIntent(actionPendingIntent)
-        .setTimeoutAfter(300000)
-        .setOngoing(true)
-        .setAutoCancel(false);
-        return builder.build();
     }
 
     public Notification getTextNotification(String category) {
@@ -139,9 +113,9 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
                 .setCustomBigContentView(notificationLayoutExpanded)
                 .setContentIntent(actionPendingIntent);
 
-        Log.e("ERROR", "Inside text notification");
         return textNotification.build();
     }
+
     public Notification getImageNotification(String category) {
         notificationLayout.setTextViewText(R.id.notification_text, "Open Me!");
         Integer content = (Integer) getContent(category);
@@ -163,15 +137,12 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     private String getRandomCategory() {
         Random random = new Random();
         int randomInt = random.nextInt(categories.size());
-        Log.e("Category selected:", categories.get(randomInt));
         return categories.get(randomInt);
     }
 
     private Object getContent(String category) {
         ArrayList<Object> contentArray = Content.contentMap.get(category);
         Random random = new Random();
-        Log.e("Content Array:", contentArray.toString());
-
         int randomInt = random.nextInt(contentArray.size());
         return contentArray.get(randomInt);
     }
@@ -180,37 +151,3 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
         return category.equals("Animals") || category.equals("Nature") || category.equals("Travel");
     }
 }
-//public class MyBroadcastReceiver extends BroadcastReceiver {
-//    public static final String NOTIFICATION_CHANNEL_ID = MyBroadcastReceiver.class.getSimpleName();
-//    public static final int NOTIFICATION_ID = 5453;
-//    @Override
-//    public void onReceive(Context context, Intent intent) {
-//        Intent actionIntent = new Intent(context, MainActivity.class);
-//
-//        PendingIntent actionPendingIntent = PendingIntent.getActivity(
-//                context,
-//                0,
-//                actionIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-//
-//        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
-//                .setSmallIcon(R.mipmap.ic_launcher)
-//                .setContentTitle("UpLift")
-//                .setContentText("Have you gone outside today? Fresh air boosts your serotonin for a better mood!")
-//                .setPriority(NotificationCompat.PRIORITY_HIGH)
-//                .setVibrate(new long[] {0, 1000})
-//                .setContentIntent(actionPendingIntent)
-//                .setTimeoutAfter(300000)
-//                .setOngoing(true)
-//                .setAutoCancel(false);
-//
-//
-//        NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID,
-//                NOTIFICATION_CHANNEL_ID+"_name",
-//                NotificationManager.IMPORTANCE_HIGH);
-//
-//        NotificationManager notifManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-//        notifManager.createNotificationChannel(channel);
-//        Log.e("ERROR", "Sending notification");
-//        notifManager.notify(NOTIFICATION_ID, builder.build());
-//    }
-//}
