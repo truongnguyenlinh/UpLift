66f57c6768644c0b1bb448291d041f5841d02c1e
krystal2694 <pwong2632my.bcit.ca>
2020-11-25 16:35:02 -0800

    dynamic notifications for the user

--- app/src/main/java/com/example/uplift/Content.java
+++ app/src/main/java/com/example/uplift/Content.java
@@ -1,11 +1,14 @@
 package com.example.uplift;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Content {
 
-public static final ArrayList<String> wellness = new ArrayList<>(
+public static final ArrayList<Object> wellness = new ArrayList<Object>(
         Arrays.asList(
                 "Have you gone outside today? Don't forget to go get some fresh air and your daily dose of Vitamin D!",
                 "Now is not the time to be hard on yourself. You are more than capable, even if you can't always see it.",
@@ -13,7 +16,7 @@ public static final ArrayList<String> wellness = new ArrayList<>(
         )
 );
 
-public static final ArrayList<String> goodNews = new ArrayList<>(
+public static final ArrayList<Object> goodNews = new ArrayList<Object>(
         Arrays.asList(
                 "Zoom is lifting its 40-minute time limit for Thanksgiving Day so families can hang out together.",
                 "After 45 years, the Gray Wolf has successfully been lifted off the US endangered species list.",
@@ -22,7 +25,7 @@ public static final ArrayList<String> goodNews = new ArrayList<>(
 );
 
 
-public static final ArrayList<String> exercise = new ArrayList<>(
+public static final ArrayList<Object> exercise = new ArrayList<Object>(
         Arrays.asList(
                 "Pace yourself. Have at least one recovery day each week to rest. If you are experiencing pain, rest until the pain has gone.",
                 "Stay hydrated. You can lose around one and a half litres of fluid for every hour of exercise; so drink water before, during and after a session.",
@@ -30,35 +33,44 @@ public static final ArrayList<String> exercise = new ArrayList<>(
         )
 );
 
-public static final ArrayList<String> animals = new ArrayList<>(
+public static final ArrayList<Object> animals = new ArrayList<Object>(
         Arrays.asList(
-                "lamb.jpg",
-                "penguin.jpg",
-                "baby_seal.jpg",
-                "bunny.jpg",
-                "cat.jpg"
+                R.drawable.lamb,
+                R.drawable.penguin,
+                R.drawable.baby_seal,
+                R.drawable.bunny,
+                R.drawable.cat
         )
 );
 
 
-public static final ArrayList<String> travel = new ArrayList<>(
+public static final ArrayList<Object> travel = new ArrayList<Object>(
         Arrays.asList(
-                "greece.jpg",
-                "iceland.jpg",
-                "maldives.jpg",
-                "whitsundays.jpg",
-                "travel1.jpg",
-                "travel2.jpg"
+                R.drawable.greece,
+                R.drawable.iceland,
+                R.drawable.maldives,
+                R.drawable.whitsundays,
+                R.drawable.travel1,
+                R.drawable.travel2
         )
 );
 
-public static final ArrayList<String> nature = new ArrayList<>(
+public static final ArrayList<Object> nature = new ArrayList<Object>(
         Arrays.asList(
-                "waterfall.jpg",
-                "joffre_lakes.jpg",
-                "northern_lights.jpg",
-                "chile.jpg",
-                "winter.jpg"
+                R.drawable.waterfall,
+                R.drawable.joffre_lakes,
+                R.drawable.nothern_lights,
+                R.drawable.chile,
+                R.drawable.winter
         )
 );
+
+public static Map<String, ArrayList<Object>> contentMap = new HashMap<String, ArrayList<Object>>() {{
+        put("Wellness", wellness);
+        put("Good News", goodNews);
+        put("Exercise", exercise);
+        put("Animals", animals);
+        put("Travel", travel);
+        put("Nature", nature);
+}};
 }
